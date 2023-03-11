5e028da140a8c13dd09b7b316e06ece35f6198cf
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-11 16:32:44 -0500

    Added the /paths and their /html pages to main app.js page. The basic html strings can be seen on web browser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import ProductListPage from "./pages/ProductListPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import CartPage from "./pages/CartPage";
 
 
 function App() {
@@ -7,7 +12,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<HomePage />} />
-    </Routes>
+        <Route path="/product-list" element={<ProductListPage />} />
+        <Route path="/product-details" element={<ProductDetailsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </BrowserRouter>
 
   );
