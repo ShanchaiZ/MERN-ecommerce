e1d059b359ce94cf4216da90d13a84d62fd8819a
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-10 19:01:09 -0500

    npm installed react-router-dom to handle routing in react apps. Imported 3 elements that handle routing in react (BrowserRouter, Routes, Route) and used those elements to route /home to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage";
 
 
 function App() {
   return (
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+    </Routes>
+    </BrowserRouter>
+
   );
 }
 
