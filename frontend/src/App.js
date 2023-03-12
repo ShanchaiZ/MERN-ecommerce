eb7a00608c2f0dac3b46bfb2e29fc5f68be93309
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-12 15:16:25 -0400

    created a protectedRoutesComponent in the src directory to simulate a user authentication access for user protected paths in app.js. Then placed all the user routes and paths under the protectedRoutes tag in app.js. Note: for the protectedRoutesComponenet, if authorization is true (meaning is user is correct user) then user can access the Outlet paths (which are /user protected paths in the main app.js). Otherwise, redirect the path to /login.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import RegisterPage from "./pages/RegisterPage";
 import CartPage from "./pages/CartPage";
 
 // User Pages:
+import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent";
 import UserProfilePage from "./pages/user/UserProfilePage";
 import UserOrdersPage from "./pages/user/UserOrdersPage";
 import UserCartDetailsPage from "./pages/user/UserCartDetailsPage";
@@ -28,10 +29,12 @@ function App() {
         {/* All other routes! */}
         <Route path="*" element="Page not found: error 404" />
         {/* User Pages that Require Login*/}
-        <Route path="/user" element={<UserProfilePage />} />
-        <Route path="/user/my-orders" element={<UserOrdersPage />} />
-        <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
-        <Route path="/user/order-details" element={<UserOrderDetailsPage />} />
+        <Route element={<ProtectedRoutesComponent />}>
+          <Route path="/user" element={<UserProfilePage />} />
+          <Route path="/user/my-orders" element={<UserOrdersPage />} />
+          <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
+          <Route path="/user/order-details" element={<UserOrderDetailsPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
 
