cf66c4508249bfa6490abfd2438ce3f57ced3152
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-12 20:17:50 -0400

    imported the Admin pages into app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,30 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import CartPage from "./pages/CartPage";
 
-// User Pages:
+// Protected User Pages:
 import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent";
 import UserProfilePage from "./pages/user/UserProfilePage";
 import UserOrdersPage from "./pages/user/UserOrdersPage";
 import UserCartDetailsPage from "./pages/user/UserCartDetailsPage";
 import UserOrderDetailsPage from "./pages/user/UserOrderDetailsPage";
 
-
+// Protected Admin Pages:
+import AdminUsersPage from "./pages/admin/AdminUsersPage";
+import AdminEditUserPage from "./pages/admin/AdminEditUserPage";
+import AdminProductsPage from "./pages/admin/AdminProductspage";
+import AdminCreateProductPage from "./pages/admin/AdminCreateProductPage";
+import AdminEditProductPage from "./pages/admin/AdminEditProductPage";
+import AdminOrdersPage from "./pages/admin/AdminOrdersPage";
+import AdminOrderDetailsPage from "./pages/admin/AdminOrderDetailsPage";
+import AdminChatsPage from "./pages/admin/AdminChatsPage";
+import AdminAnalyticsPage from "./pages/admin/AdminAnalyticsPage";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public Routes: */}
         <Route path="/home" element={<HomePage />} />
         <Route path="/product-list" element={<ProductListPage />} />
         <Route path="/product-details/:id" element={<ProductDetailsPage />} />
@@ -28,7 +38,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         {/* All other routes! */}
         <Route path="*" element="Page not found: error 404" />
-        {/* User Pages that Require Login*/}
+        {/* User Protected Routes: */}
         <Route element={<ProtectedRoutesComponent />}>
           <Route path="/user" element={<UserProfilePage />} />
           <Route path="/user/my-orders" element={<UserOrdersPage />} />
