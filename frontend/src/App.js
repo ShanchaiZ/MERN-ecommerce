e4ad58b57488cf8fe08d565bb473614d554fede2
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-12 20:34:14 -0400

    added the admin protected routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import UserOrderDetailsPage from "./pages/user/UserOrderDetailsPage";
 // Protected Admin Pages:
 import AdminUsersPage from "./pages/admin/AdminUsersPage";
 import AdminEditUserPage from "./pages/admin/AdminEditUserPage";
-import AdminProductsPage from "./pages/admin/AdminProductspage";
+import AdminProductsPage from "./pages/admin/AdminProductsPage";
 import AdminCreateProductPage from "./pages/admin/AdminCreateProductPage";
 import AdminEditProductPage from "./pages/admin/AdminEditProductPage";
 import AdminOrdersPage from "./pages/admin/AdminOrdersPage";
@@ -44,6 +44,18 @@ function App() {
           <Route path="/user/my-orders" element={<UserOrdersPage />} />
           <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
           <Route path="/user/order-details" element={<UserOrderDetailsPage />} />
+        </Route>
+        {/* Admin Protected Routes: */}
+        <Route element={<ProtectedRoutesComponent />}>
+          <Route path="/admin/users" element={<AdminUsersPage />} />
+          <Route path="/admin/edit-users" element={<AdminEditUserPage />} />
+          <Route path="/admin/products" element={<AdminProductsPage />} />
+          <Route path="/admin/create-new-product" element={<AdminCreateProductPage />} />
+          <Route path="/admin/edit-product" element={<AdminEditProductPage />} />
+          <Route path="/admin/orders" element={<AdminOrdersPage />} />
+          <Route path="/admin/order-details" element={<AdminOrderDetailsPage />} />
+          <Route path="/admin/chats" element={<AdminChatsPage />} />
+          <Route path="/admin/analytics" element={<AdminAnalyticsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
