52bb2a3cb07b2c3338cc26578cc2ce80aff854e6
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-15 16:24:35 -0400

    added react-bootstrap styling from their docs into the footer component. added custom styling in the custom stylesheet. Note: for the absolute position of the footer(child element) to work, you need relative position of HTML (parent element). The absolute position of the element must be placed inside the relative positioned element. and footer is sticky and independent of window changes

--- frontend/src/components/FooterComponent.js
+++ frontend/src/components/FooterComponent.js
@@ -1,5 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const FooterComponent = () => {
-    return <p>This is a Footer</p>
+    return (
+        <footer>
+            <Container fluid>
+                <Row className="mt-5">
+                    <Col className="bg-dark text-white text-center py-5">Copyright &copy; Shoptimize 2023</Col>
+                </Row>
+            </Container>
+        </footer>
+    );
 };
 
 export default FooterComponent;
