b3489475579fd016cb6be903057e896432b831a3
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-15 23:30:11 -0400

    added an array of categories to simulate a database. Product categories are displayed on homepage by mapping of the array of product categories.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,10 +2,24 @@ import ProductCarouselComponent from "../components/ProductCarouselComponent";
 import CategoryCardComponent from "../components/CategoryCardComponent";
 
 const HomePage = () => {
+    // Simulating a database once we have a db connected in the future below:
+    const categories = [
+        "Tablets",
+        "Monitors",
+        "Games",
+        "Software",
+        "Books",
+        "Videos",
+        "Computers"
+    ];
+
     return (
         <>
             <ProductCarouselComponent />
-            <CategoryCardComponent />
+            {
+                categories.map(() =>
+                    <CategoryCardComponent />)
+            }
         </>
     );
 };
