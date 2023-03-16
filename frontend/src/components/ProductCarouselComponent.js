b2186cb0f1ba7f027120e4ec401495e2125ac65d
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-15 23:08:46 -0400

    changed the image in carousel to redirect to product-details when clicked by placing the product title in a link container. changed the cursor style to indicate that product title in carousel image is a link

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -1,38 +1,51 @@
 import { Carousel } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
 
 const ProductCarouselComponent = () => {
+    const cursorP = {
+        cursor: "pointer"
+    }
+
     return (
         <Carousel>
+            {/* Carousel Image 1 */}
             <Carousel.Item className="carousel-img">
                 <img
                     crossOrigin="anonymous"
                     className="d-block w-100"
-                    style={{ height: "300px", objectFit:"cover" }}
+                    style={{ height: "300px", objectFit: "cover" }}
                     src="/images/carousel/carousel-1.png"
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <LinkContainer style={cursorP} to="/product-details">
+                        <h3>Bestseller In Laptops Category</h3>
+                    </LinkContainer>
+                    <p>MSI Stealth, 15.9inch HD screen</p>
                 </Carousel.Caption>
             </Carousel.Item>
+
+            {/* Carousel Image 2 */}
             <Carousel.Item className="carousel-img">
                 <img
                     className="d-block w-100"
-                    style={{ height: "300px", objectFit:"cover" }}
+                    style={{ height: "300px", objectFit: "cover" }}
                     src="/images/carousel/carousel-2.png"
                     alt="Second slide"
                 />
-
                 <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
+
+
+            {/* Carousel Image 3 */}
             <Carousel.Item className="carousel-img">
                 <img
                     className="d-block w-100"
-                    style={{ height: "300px", objectFit:"cover" }}
+                    style={{ height: "300px", objectFit: "cover" }}
                     src="/images/carousel/carousel-3.png"
                     alt="Third slide"
                 />
