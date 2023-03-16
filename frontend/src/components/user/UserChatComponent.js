3bbe0cfd705860fb402c9b9658f073e4435b5c6f
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-16 19:38:52 -0400

    replaced the console.log(id) with the user and admin p tags to simulate chat logs

--- frontend/src/components/user/UserChatComponent.js
+++ frontend/src/components/user/UserChatComponent.js
@@ -19,14 +19,17 @@ const UserChatComponent = () => {
                     <div className="cht-msg">
                         {/* JSX function that console logs the array of ids */}
                         {Array.from({ length: 20 }).map((_, id) => {
-                            console.log(id);
+                            return <>
+                                <div>
+                                    <p>
+                                        <b>You wrote:</b> This is a test Message!!
+                                    </p>
+                                    <p className="bg-primary p-3 ms-4 text-light rounded-pill">
+                                        <b>Admin Response:</b> Support Not Available! Please try again!
+                                    </p>
+                                </div>
+                            </>
                         })};
-                        <p>
-                            <b>You wrote:</b> This is a test Message!!
-                        </p>
-                        <p className="bg-primary p-3 ms-4 text-light rounded-pill">
-                            <b>Admin Response:</b> Support Not Available! Please try again!
-                        </p>
                     </div>
                     {/* Chatbot Text Area */}
                     <textarea id="clientChatMsg" className="form-control" placeholder="Enter Your Message Here!"></textarea>
