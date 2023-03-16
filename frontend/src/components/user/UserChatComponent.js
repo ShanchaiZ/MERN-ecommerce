ffd05066ddfb739bd524398155b08cfe4b19a7c9
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-16 19:03:11 -0400

    Completed chatbox form and textarea field with a submit chat button and custom css styling to chatform.

--- frontend/src/components/user/UserChatComponent.js
+++ frontend/src/components/user/UserChatComponent.js
@@ -8,10 +8,19 @@ const UserChatComponent = () => {
                 <i className="bi bi-chat-dots comment"></i>
                 <i className="bi bi-x-circle close"></i>
             </label>
-
+            {/* ChatBox Popup */}
             <div className="chat-wrapper">
                 <div className="chat-header">
                     <h6>Let's Chat - Online </h6>
+                </div>
+                {/* Chatbox Field to Enter Message */}
+                <div className="chat-form">
+                    <div className="cht-msg">
+                        <p>Chat History</p>
+                    </div>
+                    <textarea id="clientChatMsg" className="form-control" placeholder="Enter Your Message Here!"></textarea>
+                    {/* Chat Submit Button */}
+                    <button className="btn btn-primary btn-block">Submit</button>
                 </div>
             </div>
         </>
