7005cdf69234e801bdbb9e12ba9348847405b247
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-18 18:16:39 -0400

    imported and added all the FilterQueryResultOptions components in the Product List Page. Fix typo on FilterQueryResultOptions directory name

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -2,9 +2,20 @@ import PaginationComponent from "../components/PaginationComponent";
 import SortOptionsComponent from "../components/SortOptionsComponent";
 import ProductForListComponent from "../components/ProductForListComponent";
 
+import AttributesFilterComponent from "../components/filterQueryResultOptions/AttributesFilterComponent";
+import CategoryFilterComponent from "../components/filterQueryResultOptions/CategoryFilterComponent";
+import PriceFilterComponent from "../components/filterQueryResultOptions/PriceFilterComponent";
+import RatingFilterComponent from "../components/filterQueryResultOptions/RatingFilterComponent";
+
+
 const ProductListPage = () => {
     return (
         <>
+            <AttributesFilterComponent />
+            <CategoryFilterComponent />
+            <PriceFilterComponent />
+            <RatingFilterComponent />
+
             <ProductForListComponent />
             <SortOptionsComponent />
             <PaginationComponent />
