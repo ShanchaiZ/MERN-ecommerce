cd895afe86a256b2ada01c4218fcfff2053abdd4
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-19 15:56:56 -0400

    Updated PaginationComponent with Pagination example from react-bootstrap

--- frontend/src/components/PaginationComponent.js
+++ frontend/src/components/PaginationComponent.js
@@ -1,9 +1,26 @@
-
-
+import { Pagination } from "react-bootstrap";
 
 const PaginationComponent = () => {
-    return <p>this is pagination component</p>
-}
+    return (
+        <Pagination>
+          <Pagination.First />
+          <Pagination.Prev />
+          <Pagination.Item>{1}</Pagination.Item>
+          <Pagination.Ellipsis />
+    
+          <Pagination.Item>{10}</Pagination.Item>
+          <Pagination.Item>{11}</Pagination.Item>
+          <Pagination.Item active>{12}</Pagination.Item>
+          <Pagination.Item>{13}</Pagination.Item>
+          <Pagination.Item disabled>{14}</Pagination.Item>
+    
+          <Pagination.Ellipsis />
+          <Pagination.Item>{20}</Pagination.Item>
+          <Pagination.Next />
+          <Pagination.Last />
+        </Pagination>
+      );
+};
 
 
 export default PaginationComponent;
