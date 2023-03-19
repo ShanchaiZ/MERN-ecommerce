f60fe1f77521b1d583902d779413fbab1b177f36
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-19 15:52:23 -0400

    Updated the ProductForListComponent with react-bootstrap Card example but replacing the image source with one previously used for current placeholder image. Currently Only have one Product for product listing display

--- frontend/src/components/ProductForListComponent.js
+++ frontend/src/components/ProductForListComponent.js
@@ -1,8 +1,19 @@
-
-
+import { Card, Button } from "react-bootstrap";
 
 const ProductForListComponent = () => {
-    return <p>This is Product Listing Component!!!</p>
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/category/computers-category.jpg" />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+    );
 }
 
 
