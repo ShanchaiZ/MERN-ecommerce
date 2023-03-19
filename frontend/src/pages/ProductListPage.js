9a98b838dc8c0a1bd2d92ebdb3583dbb24e04a86
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-19 14:26:11 -0400

    added container fluid and column-3 to make the product listing mobile responsiveness and align left of screen. added flush variant list grouping to remove borders

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -11,10 +11,10 @@ import RatingFilterComponent from "../components/filterQueryResultOptions/Rating
 
 const ProductListPage = () => {
     return (
-        <Container>
+        <Container fluid>
             <Row>
-                <Col>
-                    <ListGroup>
+                <Col md={3}>
+                    <ListGroup variant="flush">
                         <ListGroup.Item>Cras justo odio</ListGroup.Item>
                         <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
                         <ListGroup.Item>Morbi leo risus</ListGroup.Item>
