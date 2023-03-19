02ed2e66eeb213ef0aa8307dc3b08e8488d25596
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-19 15:32:35 -0400

    Updated the product CategoryFilterComponent with react-bootstrap Form Checks and Radios. This will be updated more in the future with only checkboxes and mapping over categories

--- frontend/src/components/filterQueryResultOptions/CategoryFilterComponent.js
+++ frontend/src/components/filterQueryResultOptions/CategoryFilterComponent.js
@@ -1,7 +1,26 @@
-
+import { Form } from "react-bootstrap";
 
 const CategoryFilterComponent = () => {
-    return <p>This is the CategoryFilterComponent</p>
+    return (
+        <Form>
+            {['checkbox', 'radio'].map((type) => (
+                <div key={`default-${type}`} className="mb-3">
+                    <Form.Check
+                        type={type}
+                        id={`default-${type}`}
+                        label={`default ${type}`}
+                    />
+
+                    <Form.Check
+                        disabled
+                        type={type}
+                        label={`disabled ${type}`}
+                        id={`disabled-default-${type}`}
+                    />
+                </div>
+            ))}
+        </Form>
+    );
 };
 
 export default CategoryFilterComponent;
