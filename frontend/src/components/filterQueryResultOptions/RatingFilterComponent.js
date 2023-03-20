adc329db61b3926f75bc4d61d6fafd359612dac8
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-20 19:02:09 -0400

    added the checkbox form from react-bootstrap and npm installed and imported the react-simple-star-rating library in preparation to implement the star rating in ProductlistComponent and RatingFilterComponent

--- frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
+++ frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
@@ -1,8 +1,15 @@
-
-
+import { Form } from "react-bootstrap";
 
 const RatingFilterComponent = () => {
-    return <p>This is the RatingFilterComponent</p>
+    return (
+        <>
+            <span className="fw-Bold">Rating</span>
+            <Form.Check type="checkbox" id={`check-api-`}>
+                <Form.Check.Input type="checkbox" isValid />
+                <Form.Check.Label style={{ cursor: "pointer" }}>Enter Star Rating Here</Form.Check.Label>
+            </Form.Check>
+        </>
+    );
 };
 
 export default RatingFilterComponent;
