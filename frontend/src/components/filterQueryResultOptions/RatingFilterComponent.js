c67c216c101b1cd044d4dfd82c0f3e179b361ad8
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-20 19:09:24 -0400

    added the Star Ratings in the checkbox form for RatingFilterComponent

--- frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
+++ frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
@@ -1,12 +1,16 @@
 import { Form } from "react-bootstrap";
+import { Rating } from "react-simple-star-rating";
 
 const RatingFilterComponent = () => {
     return (
         <>
-            <span className="fw-Bold">Rating</span>
+            <span className="fw-Bold"><b>Rating</b></span>
             <Form.Check type="checkbox" id={`check-api-`}>
                 <Form.Check.Input type="checkbox" isValid />
-                <Form.Check.Label style={{ cursor: "pointer" }}>Enter Star Rating Here</Form.Check.Label>
+                <Form.Check.Label style={{ cursor: "pointer" }}>
+                    {/* Star Rating Filter: read only */}
+                    {<Rating readonly size={20} initialValue={5} />}
+                </Form.Check.Label>
             </Form.Check>
         </>
     );
