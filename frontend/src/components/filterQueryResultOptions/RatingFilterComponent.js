a6adbaa2568c22c2aa7946b23358ceb43fef8c0d
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-20 19:31:19 -0400

    implemented the star rating system in the RatingFilterComponent. Explanation of code: Since we want 5 elements of star rating and the star rating library uses an array of length 5. We execute the map function on this array (which creates a new array from calling a function for every array element). This map function takes an arrow function as a callback function and we are only returning one object (the 2nd argument of idx). The checkbox form was then returned inside the brackets of the array function. Since we want star rating displayed from 5 star to 1 star, we subtract the length of array (5) from the index of the array. this outputs 5 arrays of descending star rating starting from 5 starts, all the way to 1 star. Lastly, id of check-api is dynamic linked to the index value so each array object matches its own checkbox

--- frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
+++ frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
@@ -5,13 +5,15 @@ const RatingFilterComponent = () => {
     return (
         <>
             <span className="fw-Bold"><b>Rating</b></span>
-            <Form.Check type="checkbox" id={`check-api-`}>
-                <Form.Check.Input type="checkbox" isValid />
-                <Form.Check.Label style={{ cursor: "pointer" }}>
-                    {/* Star Rating Filter: read only */}
-                    {<Rating readonly size={20} initialValue={5} />}
-                </Form.Check.Label>
-            </Form.Check>
+            {Array.from({ length: 5 }).map((_, idx) => (
+                <Form.Check type="checkbox" id={`check-api-${idx}`}>
+                    <Form.Check.Input type="checkbox" isValid />
+                    <Form.Check.Label style={{ cursor: "pointer" }}>
+                        {/* Star Rating Filter: read only */}
+                        {<Rating readonly size={20} initialValue={5 - idx} />}
+                    </Form.Check.Label>
+                </Form.Check>
+            ))}
         </>
     );
 };
