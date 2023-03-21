efc505d34693d860ad2fe54d22f2910e5b8c1b35
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-20 20:13:32 -0400

    updated the SortBy options in the SortOptionsComponent. NOTE: the option values are given such names because it will useful in the MongoDb implementation. for example: in mongoDB, 1 -> sort in an Ascending order while -1 is Descending order.

--- frontend/src/components/SortOptionsComponent.js
+++ frontend/src/components/SortOptionsComponent.js
@@ -4,9 +4,11 @@ const SortOptionsComponent = () => {
     return (
         <Form.Select aria-label="Default select example">
             <option>Sort By</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+            <option value="price_1">Price: Low to High</option>
+            <option value="price_-1">Price: High to Low</option>
+            <option value="rating_-1">Customer Rating</option>
+            <option value="name_1">Alphabetical (A-Z)</option>
+            <option value="name_1">Reverse Alphabetical (Z-A)</option>
         </Form.Select>
     );
 }
