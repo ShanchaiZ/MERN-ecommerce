ab5290d4490e5dce19d17323368b2f21f7cc71e3
ShanchaiZ <shanchai.zahid@gmail.com>
2023-03-26 13:27:35 -0400

    imported the dismissible alert for addedtoCartMessage and created image fluid for product images in product details page

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -1,11 +1,18 @@
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Image } from "react-bootstrap";
+import AddedToCartMessageComponent from "../components/AddedToCartMessageComponent";
+
 
 const ProductDetailsPage = () => {
     return (
         <Container>
+            {<AddedToCartMessageComponent />}
             <Row className="mt-5">
                 <Col md={4}>
-                    Product Images Here
+                    {/* Product Images Here */}
+                    <Image fluid src="/images/category/tablets-category.jpg" />
+                    <Image fluid src="/images/category/games-category.jpg" />
+                    <Image fluid src="/images/category/books-category.jpg" />
+                    <Image fluid src="/images/category/computers-category.jpg" />
                 </Col>
 
                 <Col md={8}>
