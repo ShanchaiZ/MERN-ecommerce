62fe7082e8e850bcae5007b358d04a15cc82c532
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-01 18:25:36 -0400

    On the Register Page, removed the simple html h1 with Register with center styling

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,5 +1,17 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const RegisterPage = () => {
-    return <p>This is my RegisterPage!</p>
+    return (
+        <Container>
+            <Row className="mt-5 justify-content-md-center">
+                <Col md={6}>
+                    <h1>Register</h1>
+
+                </Col>
+            </Row>
+
+        </Container>
+    )
 };
 
 export default RegisterPage;
