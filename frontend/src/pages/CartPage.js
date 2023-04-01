30fcd476fbdca311662c5550e787ead17fecaa24
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-01 17:00:02 -0400

    created an array of 3 items that will be mapped onto this page that takes item as the argument and this will return CartItem Component html for now

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -6,6 +6,11 @@ const CartPage = () => {
             <Row className="mt-4">
                 <Col md={8}>
                     <h1>Shopping Cart</h1>
+                    {Array.from({length: 3}).map((item) => (
+                        <>
+                        CartItemComponent <br />
+                        </>
+                    ))}
                 </Col>
                 <Col md={4}>
                     <h3>Subtotal (2 Items)</h3>
