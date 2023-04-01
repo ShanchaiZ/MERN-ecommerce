dde6fd26e7e0d1dd5c4dc2b1db7088129043baeb
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-01 17:18:48 -0400

    expanded the Subtotal section that proceeds to checkout using linkcontainer and listgroup and added an alert

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,19 +1,36 @@
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Alert, ListGroup, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const CartPage = () => {
     return (
         <Container fluid>
             <Row className="mt-4">
+                {/* Shopping Cart Items: */}
                 <Col md={8}>
                     <h1>Shopping Cart</h1>
-                    {Array.from({length: 3}).map((item) => (
+                    {Array.from({ length: 3 }).map((item) => (
                         <>
-                        CartItemComponent <br />
+                            CartItemComponent <br />
                         </>
                     ))}
+                    <Alert variant="info">Your Cart is Empty!</Alert>
                 </Col>
+                
+                {/* Price Subtotal and Checkout: */}
                 <Col md={4}>
-                    <h3>Subtotal (2 Items)</h3>
+                    <ListGroup>
+                        <ListGroup.Item>
+                            <h3>Subtotal (2 Items)</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: <span className="fw-bold">$333</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <LinkContainer to="/user/order-details">
+                                <Button type="button">Proceed to Checkout</Button>
+                            </LinkContainer>
+                        </ListGroup.Item>
+                    </ListGroup>
                 </Col>
             </Row>
         </Container>
