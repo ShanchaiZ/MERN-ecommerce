107b804e4b7695c9dad6f666fc6d54f099ba6f9c
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-01 16:54:43 -0400

    updated the cart page with h1 and h3 inside a container

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,5 +1,18 @@
+import { Row, Col, Container } from "react-bootstrap";
+
 const CartPage = () => {
-    return <p>This is the CartPage!</p>
+    return (
+        <Container fluid>
+            <Row className="mt-4">
+                <Col md={8}>
+                    <h1>Shopping Cart</h1>
+                </Col>
+                <Col md={4}>
+                    <h3>Subtotal (2 Items)</h3>
+                </Col>
+            </Row>
+        </Container>
+    )
 };
 
 export default CartPage;
