64c9f105f7529e737141e604edaaa4e93fc2c00a
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-03 16:52:20 -0400

    recycled RegisterPage for loginPage and only email and password fields are kept

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,5 +1,81 @@
+import { Container, Row, Col, Form, Button, Spinner, Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const LoginPage = () => {
-    return <p>This is User LoginPage!</p>
+    // Form Functions Defined:
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+    return (
+        <Container>
+            <Row className="mt-5 justify-content-md-center">
+                <Col md={6}>
+                    <h1>Login</h1>
+                    {/* Client Side Form Validation from React Bootstrap Validation */}
+                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+
+                        {/* Email Address Field */}
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control
+                                required
+                                type="email"
+                                placeholder="Email Address"
+                                name="email"
+                            />
+                        </Form.Group>
+
+                        {/* Password Field */}
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                placeholder="Enter Your Password Here"
+                                name="password"
+                            />
+                            <Form.Text className="text-muted">A Password Should Have at least 8 characters!</Form.Text>
+                            <Form.Control.Feedback type="invalid">Please Enter A Valid Password</Form.Control.Feedback>
+                        </Form.Group>
+
+                        {/* User has Account Already*/}
+                        <Row className="pb-2">
+                            <Col>
+                                Do you have already have an account?
+                                <Link to={"/login"}> Login </Link> here!
+                            </Col>
+                        </Row>
+
+                        {/* Submit Button with Spinner */}
+                        <Button type="submit">
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            Submit
+                        </Button>
+
+                        <Alert show={true} variant="danger">A User with that Email already exists!</Alert>
+                        <Alert show={true} variant="success">Successfully Created A User!</Alert>
+                    </Form>
+                </Col>
+            </Row>
+
+        </Container>
+    )
 };
 
 export default LoginPage;
