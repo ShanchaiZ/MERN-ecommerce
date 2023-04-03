a1023ff67a4ac635501e389ed55dc42a56b1262c
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-03 19:41:46 -0400

    updated the UserOrderDetailsPage to have Order Details with Shipping, Payment Method, and Order Summary Columns

--- frontend/src/pages/user/UserOrderDetailsPage.js
+++ frontend/src/pages/user/UserOrderDetailsPage.js
@@ -1,5 +1,20 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const UserOrderDetailsPage = () => {
-    return <p>This is User Order Details Page!</p>
+    return (
+        <Container fluid>
+            <Row className="mt-4">
+                <h1>Order Details</h1>
+                <Col md={8}>
+                    <h2>Shipping</h2>
+                    <h2>Payment method</h2>
+                </Col>
+                <Col md={4}>
+                    <h3>Order Summary</h3>
+                </Col>
+            </Row>
+        </Container>
+    )
 };
 
 export default UserOrderDetailsPage;
