edf747eb589e18566758c391f0c62eca2c1ae958
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-03 19:04:33 -0400

    Used Tables template from react bootstrap doc in the Users Order Page

--- frontend/src/pages/user/UserOrdersPage.js
+++ frontend/src/pages/user/UserOrdersPage.js
@@ -1,5 +1,46 @@
+import { Row, Col, Table } from "react-bootstrap";
+
+
 const UserOrdersPage = () => {
-    return <p>This is User Orders Page!</p>
+    return (
+        <Row className="m-5">
+            <Col md={12}>
+                <h1>My Orders</h1>
+                {/* Bootstrap Tables */}
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td colSpan={2}>Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>
+
+
+            </Col>
+        </Row>
+    )
 };
 
 export default UserOrdersPage;
