5c6c773473099af55a6b21a2bb32c404bda51c91
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-08 17:41:54 -0400

    on admin create a new product page, added a button and a header of create a new product in a justified orientation

--- frontend/src/pages/admin/AdminCreateProductPage.js
+++ frontend/src/pages/admin/AdminCreateProductPage.js
@@ -1,5 +1,22 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const AdminCreateProductPage = () => {
-    return <p>This is Admin Create Product Page!</p>
+    return (
+        <Container className="justified-content-md-content mt-5">
+            <Row>
+                {/* Go Back to Admin Products button */}
+                <Col md={1}>
+                    <Link to="/admin/products" className="btn btn-info my-3">Go Back </Link>
+                </Col>
+                
+                {/* Create a New Product */}
+                <Col md={6}>
+                    <h1>Create a New Product</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
 };
 
 export default AdminCreateProductPage;
