30399523221a9a254797d1b35302932f379d5c78
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-09 12:59:15 -0400

    created additional fields of Product Description, Product Quantity in Stock, Price, Product Category Dropdown, Product Image Upload and Submit button

--- frontend/src/pages/admin/AdminCreateProductPage.js
+++ frontend/src/pages/admin/AdminCreateProductPage.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Form } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
@@ -11,7 +11,6 @@ const AdminCreateProductPage = () => {
             event.preventDefault();
             event.stopPropagation();
         }
-
         setValidated(true);
     };
 
@@ -22,18 +21,47 @@ const AdminCreateProductPage = () => {
                 <Col md={1}>
                     <Link to="/admin/products" className="btn btn-info my-3">Go Back </Link>
                 </Col>
-                {/* Create a New Product */}
+                {/* Create a New Product Form*/}
                 <Col md={6}>
                     <h1>Create a New Product</h1>
-
                     <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="validationCustom01">
+                        {/* Name of Product */}
+                        <Form.Group className="mb-3" controlId="formBasicName">
                             <Form.Label>Name of Product</Form.Label>
                             <Form.Control name="name" type="text" required />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                         </Form.Group>
+                        {/* Product Description */}
+                        <Form.Group className="mb-3" controlId="description">
+                            <Form.Label>Product Description</Form.Label>
+                            <Form.Control name="description" as="textarea" rows={3} required />
+                        </Form.Group>
+                        {/* Product Quantity in Stock */}
+                        <Form.Group className="mb-3" controlId="formBasicCount">
+                            <Form.Label>Number of Product in Stock</Form.Label>
+                            <Form.Control name="count" type="number" required />
+                        </Form.Group>
+                        {/* Product Price */}
+                        <Form.Group className="mb-3" controlId="formBasicPrice">
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control name="price" type="text" required />
+                        </Form.Group>
+                        {/* Product Category Dropdown */}
+                        <Form.Group className="mb-3" controlId="formBasicCategory">
+                            <Form.Label>Category</Form.Label>
+                            <Form.Select aria-label="productCategory">
+                                <option>Choose Category</option>
+                                <option value="1">Books</option>
+                                <option value="2">Laptop</option>
+                                <option value="3">Games</option>
+                            </Form.Select>
+                        </Form.Group>
+                        {/* Product Image Upload */}
+                        <Form.Group className="mb-3 mt-3" controlId="formFileMultiple">
+                            <Form.Label>Images</Form.Label>
+                            <Form.Control type="file" multiple required />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">Create</Button>
                     </Form>
-                    
                 </Col>
             </Row>
         </Container>
