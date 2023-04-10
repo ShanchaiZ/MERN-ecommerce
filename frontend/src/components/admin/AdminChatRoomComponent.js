0069c4d5ad771b377045e347e2a3f9087a3d35a0
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-10 17:30:01 -0400

    created AdminChatroom Component and imported it into AdminChatsPage with a basic row and column structure

---
+++ frontend/src/components/admin/AdminChatRoomComponent.js
@@ -0,0 +1,5 @@
+const AdminChatRoomComponent = () => {
+    return <p>This is Admin ChatsRoomComponent!</p>
+};
+
+export default AdminChatRoomComponent;
--- frontend/src/pages/admin/AdminChatsPage.js
+++ frontend/src/pages/admin/AdminChatsPage.js
@@ -1,5 +1,21 @@
+import { Row, Col } from "react-bootstrap";
+
+import AdminLinksComponent from "../../components/admin/AdminLinksComponent";
+import AdminChatRoomComponent from "../../components/admin/AdminChatRoomComponent";
+
 const AdminChatsPage = () => {
-    return <p>This is Admin Chats Page!</p>
+    return (
+        <Row className="m-5">
+            <Col md={2}>
+                <AdminLinksComponent />
+            </Col>
+            <Col md={10}>
+                <Row>
+                    <AdminChatRoomComponent />
+                </Row>
+            </Col>
+        </Row>
+    )
 };
 
 export default AdminChatsPage;
