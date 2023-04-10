caf12a89098a69e6602ecb3b5ce1f6de6245a708
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-10 18:11:36 -0400

    added a toast body with user and admin messages in styled format

--- frontend/src/components/admin/AdminChatRoomComponent.js
+++ frontend/src/components/admin/AdminChatRoomComponent.js
@@ -3,11 +3,18 @@ import { Toast } from "react-bootstrap";
 const AdminChatRoomComponent = () => {
     return (
         <>
-            <Toast>
+            <Toast className="ms-4 mb-5">
                 <Toast.Header>
                     <strong className="me-auto">Chat with John Doe</strong>
-
                 </Toast.Header>
+                <Toast.Body>
+                    <p className="bg-primary p-3 ms-4 text-light rounded-pill">
+                        <b>User Wrote:</b> This is a chat message!
+                    </p>
+                    <p>
+                        <b>Admin Wrote:</b> Understood! Have a great day!
+                    </p>
+                </Toast.Body>
 
             </Toast>
         </>
