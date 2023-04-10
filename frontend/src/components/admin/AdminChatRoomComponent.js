9c39a2c2747335b2a4e526187bbf160c22ffadea
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-10 18:28:38 -0400

    added a scroll bar in the chat box and a form text area that users can submit replies

--- frontend/src/components/admin/AdminChatRoomComponent.js
+++ frontend/src/components/admin/AdminChatRoomComponent.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Toast } from "react-bootstrap";
+import { Toast, Form, Button } from "react-bootstrap";
 
 const AdminChatRoomComponent = () => {
     return (
@@ -9,18 +9,27 @@ const AdminChatRoomComponent = () => {
                     <strong className="me-auto">Chat with John Doe</strong>
                 </Toast.Header>
                 <Toast.Body>
+                    <div style={{ maxHeight: "300px", overflow: "auto" }}>
 
-                    {Array.from({ length: 20 }).map((_, idx) => (
-                        <Fragment key={idx}>
-                            <p className="bg-primary p-3 ms-4 text-light rounded-pill">
-                                <b>User Wrote:</b> This is a chat message!
-                            </p>
-                            <p>
-                                <b>Admin Wrote:</b> Understood! Have a great day!
-                            </p>
-                        </Fragment>
-                    ))}
-                    
+                        {Array.from({ length: 20 }).map((_, idx) => (
+                            <Fragment key={idx}>
+                                <p className="bg-primary p-3 ms-4 text-light rounded-pill">
+                                    <b>User Wrote:</b> This is a chat message!
+                                </p>
+                                <p>
+                                    <b>Admin Wrote:</b> Understood! Have a great day!
+                                </p>
+                            </Fragment>
+                        ))}
+                    </div>
+                    {/* Chat Form */}
+                    <Form>
+                        <Form.Group className="mb-3" controlId="ControlTextarea">
+                            <Form.Label>Write A Message</Form.Label>
+                            <Form.Control as="textarea" rows={3}></Form.Control>
+                            <Button className="btn btn-success" type="submit">Submit</Button>
+                        </Form.Group>
+                    </Form>
                 </Toast.Body>
 
             </Toast>
