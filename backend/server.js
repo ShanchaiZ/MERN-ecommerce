79ca004ccda56a3b410b97cbfbe4dc95033c0923
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-12 21:07:26 -0400

    created another route for middlewares and added sections to help with app code organization

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,26 @@ const app = express();
 const port = 3001;
 
 
+// Middleware:
+//====================================================================
+app.use((req, res , next) =>{
+    console.log("first middleware");
+    next();
+})
 
+
+
+
+//Routes:
+//=====================================================================
 app.get("/", (req, res) => {
     res.send("Hello Shoppers!!")
 });
 
-
-
-
+app.get("/home", (req, res) => {
+    res.send("Welcome to Home Page Shoppers!!")
+    console.log("second middleware!")
+});
 
 // App is listening on port:
 //======================================================================
