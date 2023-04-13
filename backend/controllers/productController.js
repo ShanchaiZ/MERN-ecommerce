49f696fb6de1ddf10d0f8e74e0904191cd93f5d8
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-13 16:48:20 -0400

    added a controllers directory with a productController file and changed to productRoutes file to access the productController. Additional Details: A controller determines what response to send back to a user when a user makes a browser request. In the future, we send commands to database queries to access the info user wants which is productinfo.

---
+++ backend/controllers/productController.js
@@ -0,0 +1,7 @@
+
+
+const getProducts = (req, res) => {
+    res.send("Handling Product Routes! for example: product searching!");
+};
+
+module.exports = getProducts;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const getProducts = require("../controllers/productController");
 
-router.get("/", (req, res) => {
-    res.send("Handling Product Routes! for example: product searching");
-});
+router.get("/", getProducts);
 
 module.exports = router;
