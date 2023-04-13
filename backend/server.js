8623ce7cf1b818d67cd7b49a3882b801f7eb943a
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-12 22:18:02 -0400

    Created a Routes directory that will handle all the api Routes of the app and removed all the placeholder middleware and replaced it with app.use/api. All of the api requests will be handled by /api and placed in its own file

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,11 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+const apiRoutes = require("./routes/apiRoutes");
 
 // Middleware:
 //====================================================================
-app.use((req, res , next) =>{
-    console.log("first middleware");
-    next();
-})
-
+app.use("/api" , apiRoutes);
 
 
 
@@ -20,10 +17,7 @@ app.get("/", (req, res) => {
     res.send("Hello Shoppers!!")
 });
 
-app.get("/home", (req, res) => {
-    res.send("Welcome to Home Page Shoppers!!")
-    console.log("second middleware!")
-});
+
 
 // App is listening on port:
 //======================================================================
