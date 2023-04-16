b767d31dfc43dc37612e578c86a986d3743266c3
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-16 13:31:02 -0400

    Updated Product Model to have description, category, count(quantity of products), price, rating, reviewsNumber, sales, attributes(array of subcategories for the product), images, and reviews.

--- backend/models/ProductModel.js
+++ backend/models/ProductModel.js
@@ -5,7 +5,40 @@ const productSchema = mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-    }
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    count: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    reviewsNumber: {
+        type: Number
+    },
+    sales: {
+        type: Number,
+        default: 0,
+    },
+    attrs: [
+        //a product in a category can be further classified by -> [{key:"color", value: "red"}, {key: "size", value: "1 TB"}]
+        { key: { type: String }, value: { type: String } }
+    ],
+    images: [],
+    reviews: []
 }, {
     timestamps: true,
 })
