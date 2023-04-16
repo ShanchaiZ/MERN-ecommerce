51bf236bf38d93b112956cf94adcecd6b26d3e9c
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-16 13:56:19 -0400

    created Reviews Model with comment, rating, user (which uses mongoose built in id and name) and timestamps properties in the Model directory

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,6 @@
-const Product = require("../models/ProductModel");
 
 
 const getProducts = (req, res) => {
-    Product.create({name:"Toshiba"});
     res.send("Handling Product Routes! for example: product searching!");
 };
 
