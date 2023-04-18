a2a64ffb4933572bcc44e98d8b44c6b89ef19058
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-18 15:57:02 -0400

    created seed data of categories in seeder directory

---
+++ backend/seeder/categories.js
@@ -0,0 +1,68 @@
+const categories = [
+    {
+        name: "Computers",
+        description:
+            "Computers - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+        image: "/images/category/computers-category.jpg",
+        attrs: [
+            { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
+            { key: "color", value: ["blue", "red", "green", "black"] },
+        ],
+    },
+    {
+        name: "Tablets",
+        description:
+            "Tablets - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+        image: "/images/tablets-category.jpg",
+        attrs: [
+            { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
+            { key: "color", value: ["blue", "red", "green", "black"] },
+        ],
+    },
+    {
+        name: "Videos",
+        description:
+            "Videos - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+        image: "/images/videos-category.jpg",
+        attrs: [
+            { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
+            { key: "color", value: ["blue", "red", "green", "black"] },
+        ],
+    },
+    {
+        name: "Books",
+        description:
+            "Books - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+        image: "/images/books-category.jpg",
+        attrs: [
+            { key: "genre", value: ["movie", "comedy", "thriller"] }
+        ],
+    },
+    {
+        name: "Software",
+        description:
+            "Softwares - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+        image: "/images/category/software-category.jpg",
+    },
+    {
+        name: "Electronics",
+        description:
+            "Electronics - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+        image: "/images/category/electronics-category.jpg",
+    },
+    {
+        name: "Games",
+        description:
+            "Games - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+        image: "/images/category/games-category.jpg",
+    },
+    {
+        name: "Monitors",
+        description:
+            "Monitors - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+        image: "/images/category/monitors-category.jpg",
+    },
+]
+
+
+module.exports = categories;
