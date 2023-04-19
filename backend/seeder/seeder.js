fefb64105b78f136d85c5c5cc853c577957c1b93
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-18 23:08:14 -0400

    added product seeder in the product seeders file

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -1,14 +1,31 @@
 const connectDB = require("../config/db");
 connectDB();
 
+// Imported the Seeder files:
 const categoryData = require("./categories");
+const productData = require("./products");
+
+
+// Imported the Seeder Models:
 const Category = require("../models/CategoryModel");
+const Product = require("../models/ProductModel");
+
 
 const importData = async () => {
     try {
-        // Category.collection.dropIndexes();
+        // Drop Seeder Indexes:
+        // await Category.collection.dropIndexes();
+        // await Product.collection.dropIndexes();
+
+        // Delete Seeder Collections:
         await Category.collection.deleteMany({});
+        await Product.collection.deleteMany({});
+
+        // Create Seeder Collection:
         await Category.collection.insertMany(categoryData);
+        await Product.collection.insertMany(productData);
+
+
         console.log("Data Seeding is Successful!");
         process.exit();
     } catch (error) {
