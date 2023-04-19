2c62c15a34f45169622045f493f1f067451d046c
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-19 17:41:30 -0400

    added reviews seeder to product seeder file. removed collection infront of insertmany method because we want to add many documents of products, category and reviews NOT collection of products,category and reviews.  Solved issue by adding 'new' in front of objectID on reviews seeder due to TypeError: Class constructor cannot be invoked without 'new'.

--- backend/seeder/reviews.js
+++ backend/seeder/reviews.js
@@ -4,27 +4,27 @@ const reviews = [
     {
         comment: "Review 1. Lorem ipsum dolor sit amet. Et officia beatae vel unde error non fuga magni ut error odio qui tempora laboriosam.",
         rating: 5,
-        user: { _id: ObjectId(), name: "John Doe" }
+        user: { _id: new ObjectId(), name: "John Doe" }
     },
     {
         comment: "Review 2. Lorem ipsum dolor sit amet. Et officia beatae vel unde error non fuga magni ut error odio qui tempora laboriosam.",
         rating: 5,
-        user: { _id: ObjectId(), name: "John Doe" }
+        user: { _id: new ObjectId(), name: "John Doe" }
     },
     {
         comment: "Review 3. Lorem ipsum dolor sit amet. Et officia beatae vel unde error non fuga magni ut error odio qui tempora laboriosam.",
         rating: 5,
-        user: { _id: ObjectId(), name: "John Doe" }
+        user: { _id: new ObjectId(), name: "John Doe" }
     },
     {
         comment: "Review 4. Lorem ipsum dolor sit amet. Et officia beatae vel unde error non fuga magni ut error odio qui tempora laboriosam.",
         rating: 5,
-        user: { _id: ObjectId(), name: "John Doe" }
+        user: { _id: new ObjectId(), name: "John Doe" }
     },
     {
         comment: "Review5. Lorem ipsum dolor sit amet. Et officia beatae vel unde error non fuga magni ut error odio qui tempora laboriosam.",
         rating: 5,
-        user: { _id: ObjectId(), name: "John Doe" }
+        user: { _id: new ObjectId(), name: "John Doe" }
     }
 ]
 
--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,11 +4,13 @@ connectDB();
 // Imported the Seeder files:
 const categoryData = require("./categories");
 const productData = require("./products");
+const reviewData = require("./reviews");
 
 
 // Imported the Seeder Models:
 const Category = require("../models/CategoryModel");
 const Product = require("../models/ProductModel");
+const Review = require("../models/ReviewModel");
 
 
 const importData = async () => {
@@ -16,14 +18,17 @@ const importData = async () => {
         // Drop Seeder Indexes:
         // await Category.collection.dropIndexes();
         // await Product.collection.dropIndexes();
+        // await Review.collection.dropIndexes();
 
         // Delete Seeder Collections:
         await Category.collection.deleteMany({});
         await Product.collection.deleteMany({});
+        await Review.collection.deleteMany({});
 
         // Create Seeder Collection:
-        await Category.collection.insertMany(categoryData);
-        await Product.collection.insertMany(productData);
+        await Category.insertMany(categoryData);
+        await Product.insertMany(productData);
+        await Review.insertMany(reviewData);
 
 
         console.log("Data Seeding is Successful!");
