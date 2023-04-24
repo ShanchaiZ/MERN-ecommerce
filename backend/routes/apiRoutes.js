05657d36377b4bc7dc124d2b5b43c177305ad17d
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-24 16:46:03 -0400

    created api routes for category, order, and users similar to products in routes directory

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
-const productRoutes = require("./productRoutes");
 
-app.use("/products", productRoutes)
+// Imported File for Path:
+// ====================================================================
+const productRoutes = require("./productRoutes");
+const categoryRoutes = require("./categoryRoutes");
+const userRoutes = require("./userRoutes");
+const orderRoutes = require("./orderRoutes");
+
+// Middleware:
+// ====================================================================
+app.use("/products", productRoutes);
+app.use("/categories", categoryRoutes);
+app.use("/users", userRoutes);
+app.use("/orders", orderRoutes);
 
 module.exports = app;
