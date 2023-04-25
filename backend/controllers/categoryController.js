436a3d77bcc530257ea3fdf4403d8c7f7724bc7d
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-25 16:28:47 -0400

    added a POST route and controller for creating a new category. 1 exclamation mark = turns the req.body into a boolean value (which is TRUE).  2 exclamation marks = gives the opposite the value of exclamation req.body which is FALSE. additional note: If req.body is empty, 1 exclamation mark turns the req.body into a boolean value, which is TRUE because empty value is represented by FALSE. this is used to check the boolean value of any data.

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -2,13 +2,23 @@
 const Category = require("../models/CategoryModel");
 
 
+// GET Request: Find all categories
 const getCategories = async (req, res, next) => {
     try {
-        const cagetories = await Category.find({}).sort({name: "asc"}).orFail();
+        const cagetories = await Category.find({}).sort({ name: "asc" }).orFail();
         res.json(cagetories);
     } catch (error) {
         next(error);
     }
 };
 
-module.exports = getCategories;
+//POST Request: Create a new Category
+const newCategory = async (req, res, next) => {
+    try {
+        res.send(!!req.body); //1 exclamation mark = turns the req.body into a boolean value (which is TRUE).  2 exclamation marks = gives the opposite the value of !req.body which is FALSE.
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports =  {getCategories, newCategory};
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const getCategories = require("../controllers/categoryController");
+const { getCategories, newCategory } = require("../controllers/categoryController");
 
+//GET ROUTE: Find all Categories:
 router.get("/", getCategories);
+
+// POST ROUTE: Create a New Category:
+router.post("/", newCategory);
 
 module.exports = router;
