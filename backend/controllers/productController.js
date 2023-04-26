78f1c2a49585c6c168b9db162b65dcd10ce72b16
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-26 15:15:23 -0400

    created api for getting Products from database by importing Product Model, awaiting the search for all product with sorting and displaying the result as a json object

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,12 @@
+const Product = require("../models/ProductModel");
 
-
-const getProducts = (req, res) => {
-    res.send("Handling Product Routes! for example: product searching!");
+const getProducts = async (req, res, next) => {
+    try {
+        const products = await Product.find({}).sort({ name: 1 }) //instead of "asc" string to sort in name in ascending order, 1 also works! Note: -1 = descending order
+        res.json({ products });
+    } catch (error) {
+        next(error)
+    }
 };
 
 module.exports = getProducts;
