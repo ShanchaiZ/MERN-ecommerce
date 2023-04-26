a8ea430581a81e0c964d7e3b62ab3fd13f7d4da8
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-26 15:22:56 -0400

    added the limit method for product that control the amount of products that can be displayed in an array. created and imported the recordsPerPage as a separate config to be used for pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,13 @@
 const Product = require("../models/ProductModel");
+const recordsPerPage = require("../config/pagination");
 
 const getProducts = async (req, res, next) => {
     try {
-        const products = await Product.find({}).sort({ name: 1 }) //instead of "asc" string to sort in name in ascending order, 1 also works! Note: -1 = descending order
+        const products = await Product
+            .find({}) //finds all products in the db hence the empty {}
+            .sort({ name: 1 })//instead of "asc" string to sort in name in ascending order, 1 also works! Note: -1 = descending order
+            .limit(recordsPerPage); //limits the amount of product results that can be displayed; used for pagination
+
         res.json({ products });
     } catch (error) {
         next(error)
