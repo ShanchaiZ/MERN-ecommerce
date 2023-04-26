228668190157fb729be323e8ac6194f93a8a8a5c
ShanchaiZ <shanchai.zahid@gmail.com>
2023-04-26 16:32:26 -0400

    added the skip method that skips over number of records that are displayed on the page number for pagination. for example: if pageNum = 1, then (1-1 = 0) meaning .skip (0) which means no product results are being skipped over in the first page. for example: if pageNum = 2, then (2-1 = 1) meaning recordsPerPage = 2 * 1 = 2 meaning we are skipping over 2 products on page 2. for example: if we are on large page = 30 then since there are no product to be displayed. its all skipped with no resulting product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,10 +3,15 @@ const recordsPerPage = require("../config/pagination");
 
 const getProducts = async (req, res, next) => {
     try {
+        const pageNum = Number(req.query.pageNum) || 1
+        // res.json({pageNum});
+
         const products = await Product
+
             .find({}) //finds all products in the db hence the empty {}
             .sort({ name: 1 })//instead of "asc" string to sort in name in ascending order, 1 also works! Note: -1 = descending order
-            .limit(recordsPerPage); //limits the amount of product results that can be displayed; used for pagination
+            .limit(recordsPerPage) //limits the amount of product results that can be displayed; used for pagination
+            .skip(recordsPerPage * (pageNum - 1)); //skips over the number of records in the argument and displays results after the skipped number indicated by recordsPerPage
 
         res.json({ products });
     } catch (error) {
