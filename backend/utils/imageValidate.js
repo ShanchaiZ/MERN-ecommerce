6c02f9b28ceb80469ebc4f140b373fb63bcb14aa
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-03 20:24:55 -0400

    added allowed image upload of 1MB and allow file format of jpg, jpeg, and png. Note: filetype code line is a regular expression

--- backend/utils/imageValidate.js
+++ backend/utils/imageValidate.js
@@ -6,7 +6,23 @@ const imageValidate = (images) => {
         imagesTable.push(images)
     }
 
+    // Image Upload configuration:
     if (imagesTable.length > 3) {
         return { error: "Please upload maximum of 3 images!" }
     }
+    for (let image of imagesTable) {
+        // 1 MB = 1048576 bytes
+        if (image.size > 1048576) return { error: "Image size too large (above 1 MB)" }
+        
+        // Allowed mimetype/file extensions for image upload:
+        const filetypes = /jpg|jpeg|png/
+        const mimetype = filetypes.test(image.mimetype);
+        if (!mimetype) return { error: "Incorrect file extension (should be jpg, jpeg, or png)" }
+    }
+
+
+    return { error: false }
 }
+
+
+module.exports = imageValidate;
