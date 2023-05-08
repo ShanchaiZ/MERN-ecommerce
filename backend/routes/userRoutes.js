54e79a61f9f9bc4fee6142933dbe18172184ef0c
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-08 15:37:04 -0400

    Created a register New User route using the Imported User Model. Explanation of code: 1.Try catch block validation created which takes in name, lastname, email and password from req.body. if the listed fields are missing then server responds with error 400. 2.If there is a user with the same email, then respond with server error 400 3.Otherwise: create a user using the imported User Model, with server status 201 4.Then export the newly made function. Note: password will not stored in plain text like this because this is a security risk and will be hashed. the user Routers are reorganized under user routes and admin routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const getUsers = require("../controllers/userController");
+const { getUsers, registerUser } = require("../controllers/userController");
 
+
+// USER ROUTES:
+// ===================================================================================================
+router.post("/register", registerUser);
+
+
+// ADMIN ROUTES:
+// ===================================================================================================
+// GET Route: Find all users:
 router.get("/", getUsers);
 
 module.exports = router;
