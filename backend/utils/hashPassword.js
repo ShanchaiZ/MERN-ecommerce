437084ed376451252145301a02a9a928960076eb
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-09 17:18:26 -0400

    created password comparing function using bcrypt method that takes in 2 arguments of input password and hashed password to be exported

--- backend/utils/hashPassword.js
+++ backend/utils/hashPassword.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcryptjs");
 const salt = bcrypt.genSaltSync(10);
 
+// Hashing a password with salt:
 const hashPassword = password => bcrypt.hashSync(password, salt);
 
-module.exports = { hashPassword };
+// Comparing Passwords (requires 2 argument of input password and hashed password) to be compared:
+const comparePasswords = (inputPassword, hashedPassword) => bcrypt.compareSync(inputPassword, hashedPassword);
+
+module.exports = { hashPassword, comparePasswords };
