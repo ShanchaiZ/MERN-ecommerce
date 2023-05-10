d13881e8502136ae868b1e2afae76f2069eec493
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-09 21:38:00 -0400

    Created functionality that verifies when a user is logged in that they have a token for that session and that they have the correct token to be logged into the session. explanation of code: 1.Previous commit was to Create a custom middleware directory. This middleware is used to verify if a user is logged in as admin. Note: next() is necessary because it will allow the middleware to run or it will count as a terminal endpoint. 2.Create a token variable that checks if the request has a cookie named access_token. Note: by default express can not read cookies. 3.Npm i and require ‘cookie-parser’ in the main server.js file. Then use it as a middleware in main server file. Now when the endpoint is hit, a cookie with user data is encoded in the console. 4.Now we check if token exists. If no token then Error 403 (User does not have permission to access this resource). Cookie is deleted from API testing platform to verify - works 5.Now that token existence is verified, need to decode the user info from it. Imported jwt and verify the jwt verification that takes in the encoded user data in token for and jwt secret key and compares it. 6.If it’s the correct jwt token, then decoded jwt token gets assigned to all of the current user’s request. This was tested in API testing platform by modifying the token cookie  which denied user access. user regained access once cookie modification was reverted

--- backend/middleware/verifyAuthToken.js
+++ backend/middleware/verifyAuthToken.js
@@ -1,9 +1,23 @@
-
+const jwt = require("jsonwebtoken");
 
 const verifyIsLoggedIn = (req, res, next) => {
     try {
+        // Is there a Token?:
         const token = req.cookies.access_token;
-        console.log(token);
+        if (!token) {
+            return res.status(403).send("A token is required for authentication!");
+        }
+
+        //Verify if correct token:
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            req.user = decoded;
+            return next();
+        } catch (error) {
+            return res.status(401).send("Unauthorized Access. Invalid Token");
+        }
+
+
         next();
     } catch (error) {
         next(error);
