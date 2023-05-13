71b7c5d1ea9e499332408dfc8d6d1c2631545f89
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-13 18:08:53 -0400

    Created api endpoint that creates/saves orders in database. more explanation of code: 2.We map through the cartItems and map through it and through each item we will return the product Id. We place all the productID in the cart under a ids array therefore, ‘ids’ is the collection of ID of product ID numbers. We repeat this process with quantity (where qty is a collection of item quantity). 3.we then find all products ids whose ID includes (represented by mongodb db operator ) in ids array. 4. Then we create an anonymous forEach function that takes in argument of product and idx, we want to increase quantity number from index. This means that quantity length is the same as product length. 5.Then product.save to update the product. So the sales property will be updated for each individual added to the cart. 6.To create New order using the cart items, a new order object was created with user (this user id is from Object id from database), orderTotal, cartItems, and paymentMethod. 7.The New Order object that has product and cartItems are now saved in database with a server status 201 which means new resource created.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 const Order = require("../models/OrderModel");
 const ObjectId = require("mongodb").ObjectId;
+const Product = require("../models/ProductModel");
 
 
 // Get All orders for a specific User:
@@ -26,15 +27,44 @@ const getOrder = async (req, res, next) => {
 // Create an Order:
 const createOrder = async (req, res, next) => {
     try {
+        // An Order requires CartItems, OrderTotal and PaymentMethod:
         const { cartItems, orderTotal, paymentMethod } = req.body;
         if (!cartItems || !orderTotal || !paymentMethod) {
             return res.status(400).send("All fields are required!");
         }
+
+        // Take the Product item's ID and place in ids array
+        let ids = cartItems.map((item) => {
+            return item.productID;
+        });
+        // Take the Quantity of items and place in qty array 
+        let qty = cartItems.map((item) => {
+            return Number(item.quantity);
+        });
+
+        // Find each product that is $included in the ids array then increase the quantity under product sales then save as new product.
+        await Product.find({ _id: { $in: ids } }).then((products) => {
+            products.forEach(function (product, idx) {
+                product.sales += qty[idx];
+                product.save();
+            })
+        })
+
+        // Once product items are totaled place inside a new order object with such criteria below:
+        const order = new Order({
+            user: new ObjectId(req.user._id),
+            orderTotal: orderTotal,
+            cartItems: cartItems,
+            paymentMethod: paymentMethod
+        })
+
+        // Then save order:
+        const createdOrder = await order.save();
+        res.status(201).send(createdOrder);
     } catch (error) {
         next(error);
     }
-}
-
+};
 
 
 module.exports = { getUserOrders, getOrder, createOrder };
