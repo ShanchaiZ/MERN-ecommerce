8373b990d2575416027b7093d23476e0ca33816c
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-13 12:08:37 -0400

    prepared fetching orders for regular user by reorganizing the orderRoutes and renaming GetUserOrders in ordersController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,7 @@
 
 
-const getOrders = (req, res) => {
+const getUserOrders = async (req, res, next) => {
     res.send("Handling Order Routes, Like getting orders!");
 };
 
-module.exports = getOrders;
+module.exports = getUserOrders;
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const getOrders = require("../controllers/orderController");
+const { verifyIsLoggedIn, verifyIsAdmin } = require("../middleware/verifyAuthToken");
+const getUserOrders = require("../controllers/orderController");
 
-router.get("/", getOrders);
+
+// USER PROTECTED ROUTES:
+// ===================================================================================================
+// Verify if user is logged in:
+router.use(verifyIsLoggedIn);
+
+// GET Route: User Fetches their orders:
+router.get("/", getUserOrders);
+
+//PROTECTED ADMIN ROUTES:
+// ===================================================================================================
+// Verify if user is logged in AS AN ADMIN:
+router.use(verifyIsAdmin);
+
+
 
 module.exports = router;
