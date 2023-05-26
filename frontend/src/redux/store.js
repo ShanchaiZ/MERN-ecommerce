99cab20b158660757b5819b949211a1e51e85ee4
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-26 16:55:27 -0400

    To understand the Redux concepts: Store.dispatch is a method that is used to call the actions that CHANGES the global state.and it takes and object with properties as the argument. In this case we added a type 'ADD' and an arbitrary value of 10. In the reducer we added a second argument 'action'. Then in the reducer, we create a case where if 'Add' is a state that the app is in, we return the default state ( which is assigned to be 0 initially) which is the old state, then we add +1 to the old state then add the action which is dispatched to be 10. Therefore in the output, the return value = 11 when state is changed and it is available on all the webpages. In addition, a default state is returned.

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,26 @@
 import { legacy_createStore as createStore } from "redux";
 
-const counterReducer = (state = { value: 0 }) => {
-    return state;
+
+// Reducer: are functions in Redux are used in changing a state:
+const counterReducer = (state = { value: 0 }, action) => {
+    switch (action.type) {
+        case "ADD":
+            return { value: state.value + 1 + action.someValue };
+        default:
+            return state;
+    }
 }
 
+
 const store = createStore(counterReducer, { value: 0 });
+
+
+// Store.dispatch: used to call the actions that CHANGES the global state:
+store.dispatch({
+    type: "ADD",
+    someValue: 10
+});
+
 
 
 console.log(store.getState());
