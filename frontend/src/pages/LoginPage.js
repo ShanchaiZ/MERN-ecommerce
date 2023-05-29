5b8898fc9e9ebf1cc1e922e6ff144928a6881dcf
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-29 16:55:06 -0400

    To use the newly created user login action, it is called in the LoginPage logic page. The useDispatch Redux keyword and the setReduxUserState action are imported to be passed as login page properties for the loginPageComponent. NOTE: the userlogin register Reducer should ideally be called in the loginPage component but is not done to make the component testing using Jest a easier.

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -3,6 +3,11 @@ import LoginPageComponent from "./components/LoginPageComponent";
 // Axios used to make API Calls:
 import axios from "axios";
 
+// Action: is called using 'dispatch' keyword:
+import { useDispatch } from "react-redux";
+import { setReduxUserState } from "../redux/actions/userActions";
+
+
 const loginUserApiRequest = async (email, password, doNotLogout) => {
     const { data } = await axios.post("/api/users/login", { email, password, doNotLogout })
     return data;
@@ -11,8 +16,8 @@ const loginUserApiRequest = async (email, password, doNotLogout) => {
 
 
 const LoginPage = () => {
-    
-    return <LoginPageComponent loginUserApiRequest={loginUserApiRequest} />
+    const reduxDispatch = useDispatch();
+    return <LoginPageComponent loginUserApiRequest={loginUserApiRequest} reduxDispatch={reduxDispatch} setReduxUserState={setReduxUserState} />
 };
 
 export default LoginPage;
