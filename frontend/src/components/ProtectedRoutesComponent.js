fda3e897d8a2b4803428d62ec18cd666a44cec08
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-29 22:21:16 -0400

    Created authorization logic for user and admin based on login credentials. Explanation of code: imported axios, React statehooks and LoginPage. Then defined authorization logic based on regular user, admin user and not a user. if Not a User(user not logged in) then redirected to login page. If User is logged in AND is admin, then user has access to admin routes. Otherwise, if logged in User is a regular user then user has access to the User chat and user routes. If all else, then redirected to login page.

--- frontend/src/components/ProtectedRoutesComponent.js
+++ frontend/src/components/ProtectedRoutesComponent.js
@@ -1,16 +1,31 @@
 import { Outlet, Navigate } from "react-router-dom";
 import UserChatComponent from "./user/UserChatComponent";
 
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import LoginPage from "../pages/LoginPage";
+
+
 const ProtectedRoutesComponent = ({ admin }) => {
 
-    if (admin) {
-        let adminAuth = true;
-        return adminAuth ? <Outlet /> : <Navigate to="/login" />;
-    } else {
-        let userAuth = true;
-        return userAuth ? <> <UserChatComponent /><Outlet /> </> : <Navigate to="/login" />;
-    }
+    const [isAuth, setIsAuth] = useState();
 
+    // User Not Logged in, Redirect to Loginpage:
+    if (isAuth === undefined) return <LoginPage />
+
+    // Login Logic:
+    return isAuth && admin && isAuth !== "admin" ? (    // If User is Logged in and is NOT Admin...:
+        <Navigate to="/login" />        // ...Redirect to login route...:
+    ) : isAuth && admin ? (// ...Otherwise, if user Is logged and Admin, Show all the admin protected routes.
+        <Outlet />
+    ) : isAuth && !admin ? ( //If User is logged and is not an admin (regular User)..then show User Chat and User Routes...
+        <>
+            <UserChatComponent />
+            <Outlet />
+        </>
+    ) : (
+        <Navigate to="/login" /> //...Otherwise, if all else, redirect to login page.
+    )
 };
 
 export default ProtectedRoutesComponent;
