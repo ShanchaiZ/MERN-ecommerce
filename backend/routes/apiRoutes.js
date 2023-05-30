0db021006280775cfef00aa87e7bbe3c795452c6
ShanchaiZ <shanchai.zahid@gmail.com>
2023-05-29 22:32:46 -0400

    added a get-token route for Authorized Access to Proper Routes for all api Routes. So now, all api routes require a verified cookie.

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -8,6 +8,19 @@ const categoryRoutes = require("./categoryRoutes");
 const userRoutes = require("./userRoutes");
 const orderRoutes = require("./orderRoutes");
 
+// Session Cookies for Authorized Access to Routes:
+const jwt = require("jsonwebtoken");
+app.get("/get-token", (req, res) => {
+
+    try {
+        const accessToken = req.cookies["access_token"];
+        const decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
+        return res.json({ token: decoded.name, isAdmin: decoded.isAdmin });
+    } catch (error) {
+        return res.status(401).send("Unauthorized Access. Invalid Token!");
+    }
+});
+
 // Middleware:
 // ====================================================================
 app.use("/products", productRoutes);
