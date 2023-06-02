7cc0f4a98d8d8b0c4f0ba9a943e85d75ce4bccd4
ShanchaiZ <shanchai.zahid@gmail.com>
2023-06-02 18:11:31 -0400

    moved Update User Profile using fields outside of the UserProfilePage function. created function that Fetches User Data for Easier Profile Update and passed through the React Component as a prop. but where do we find the user_id? found in the React Redux state during login.

--- frontend/src/pages/user/UserProfilePage.js
+++ frontend/src/pages/user/UserProfilePage.js
@@ -3,16 +3,24 @@ import UserProfilePageComponent from "./components/UserProfilePageComponent";
 // Axios Used to make API Calls:
 import axios from "axios";
 
+// Function for Updating User Profile using the fields listed below:
+const updateUserApiRequest = async (name, lastName, phoneNumber, address, country, zipCode, city, state, password) => {
+    const { data } = await axios.put("/api/users/profile", { name, lastName, phoneNumber, address, country, zipCode, city, state, password })
+    return data;
+};
+
+
+// Fetching User Data for Easier Profile Update:
+const fetchUser = async (user_id) => {
+    const { data} = await axios.get("/api/user/profile/" + user_id);
+    return data;
+}
+
 
 // Put Method used to Update User Profile:
 const UserProfilePage = () => {
 
-    const updateUserApiRequest = async (name, lastName, phoneNumber, address, country, zipCode, city, state, password) => {
-        const { data } = await axios.put("/api/users/profile", { name, lastName, phoneNumber, address, country, zipCode, city, state, password })
-        return data;
-    }
-
-    return <UserProfilePageComponent updateUserApiRequest={updateUserApiRequest} />
+    return <UserProfilePageComponent updateUserApiRequest={updateUserApiRequest} fetchUser={fetchUser} />
 };
 
 export default UserProfilePage;
