2160f38f0e14308c44b6837a2b63b67e58a308d4
ShanchaiZ <shanchai.zahid@gmail.com>
2023-06-03 19:40:58 -0400

    updated the cart actions to call on function that fetches particular product data and call dispatch which calls the reducer.

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -1,13 +1,22 @@
 import * as actionTypes from "../constants/cartConstants";
 
+import axios from "axios";
 
 // Store.dispatch: used to call the actions that CHANGES the global state:
-export const addToCart = (productId, quantity) => (dispatch) =>{
-    console.log(productId);
-    console.log(quantity);
-    
+export const addToCart = (productId, quantity) => async (dispatch) => {
+
+    // Api call to fetch product id of a product when add to cart action is called:
+    const { data } = await axios.get(`/api/products/get-one/${productId}`);
+
     dispatch({
         type: actionTypes.ADD_TO_CART,
-        someValue: 0,
+        payload: {
+            productID: data._id,
+            name: data.name,
+            price: data.price,
+            image: data.image[0] ?? null,
+            count: data.count,
+            quantity,
+        },
     })
 }
