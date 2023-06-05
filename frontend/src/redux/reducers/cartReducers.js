dc3cce5d52e80e82637759e58042997500dce53c
ShanchaiZ <shanchai.zahid@gmail.com>
2023-06-04 21:13:08 -0400

    updated the add to cart reducer. explanation of code: products that are saved in redux global state are products ID and cart items. The currentstate of redux with all the items are available through destructuring. If the product exist in redux global state then set item, subtotal to 0 (so we can update on it with newly inputted values) else, display 'x' (which is dummy value added to see if state is changed. will be updated later to reflect # of items, subtotal) then update the current state of redux instead of using the initial state

--- frontend/src/redux/reducers/cartReducers.js
+++ frontend/src/redux/reducers/cartReducers.js
@@ -11,8 +11,22 @@ const CART_INITIAL_STATE = {
 export const cartReducer = (state = CART_INITIAL_STATE, action) => {
     switch (action.type) {
         case actionTypes.ADD_TO_CART:
-            console.log(action.payload);
-            return state;
+            const productBeingAddedToCart = action.payload;
+
+            const productAlreadyExistsInState = state.cartItems.find((x) => x.productID === productBeingAddedToCart.productID);
+
+            const currentState = { ...state }; //Make a copy of state to be changed
+
+            if (productAlreadyExistsInState) {
+                currentState.itemsCount = 0;
+                currentState.cartSubtotal = 0;
+                currentState.cartItems = productAlreadyExistsInState;
+            } else {
+                currentState.itemsCount = "x"; //dummy value added to see if state is changed. will be updated later to reflect # of  items 
+                currentState.cartSubtotal = "x"; //dummy value added to see if state is changed. will be updated later to reflect total cost
+            }
+
+            return currentState;
         default:
             return state;
     }
