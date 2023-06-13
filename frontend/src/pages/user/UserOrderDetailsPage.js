c81cf83935c9b7dbb80cf2b28c6f543711727c56
ShanchaiZ <shanchai.zahid@gmail.com>
2023-06-12 21:18:18 -0400

    created a getOrder Request from database that dynamically show orders from the shopping cart to this page api call and passed into the component as a prop

--- frontend/src/pages/user/UserOrderDetailsPage.js
+++ frontend/src/pages/user/UserOrderDetailsPage.js
@@ -4,6 +4,12 @@ import { useSelector } from "react-redux";
 
 import axios from "axios";
 
+// Get Order from Database and dynamically show on this page:
+const getOrder = async (orderId) => {
+    const { data } = await axios.get("/api/orders/user/" + orderId);
+    return data;
+}
+
 const UserOrderDetailsPage = () => {
     const userInfo = useSelector((state) => state.userRegisterLogin.userInfo);
 
@@ -13,7 +19,7 @@ const UserOrderDetailsPage = () => {
     }
 
 
-    return <UserOrderDetailsPageComponent userInfo={userInfo} getUser={getUser} />
+    return <UserOrderDetailsPageComponent userInfo={userInfo} getUser={getUser} getOrder={getOrder} />
 };
 
 export default UserOrderDetailsPage;
