c6fbb6db7227950e28a9aae62c0a2630fbbdb208
ShanchaiZ <shanchai.zahid@gmail.com>
2023-06-12 20:47:21 -0400

    created a getUser Profile from database api call and passed it to the component

--- frontend/src/pages/user/UserOrderDetailsPage.js
+++ frontend/src/pages/user/UserOrderDetailsPage.js
@@ -2,9 +2,18 @@ import UserOrderDetailsPageComponent from "./components/UserOrderDetailsPageComp
 
 import { useSelector } from "react-redux";
 
+import axios from "axios";
+
 const UserOrderDetailsPage = () => {
     const userInfo = useSelector((state) => state.userRegisterLogin.userInfo);
-    return <UserOrderDetailsPageComponent userInfo={userInfo} />
+
+    const getUser = async () => {
+        const { data } = await axios.get("/api/users/profile/" + userInfo._id);
+        return data;
+    }
+
+
+    return <UserOrderDetailsPageComponent userInfo={userInfo} getUser={getUser} />
 };
 
 export default UserOrderDetailsPage;
