5eba887a0cc858e5536858333455192e0ae3a875
ShanchaiZ <shanchai.zahid@gmail.com>
2023-06-21 18:47:20 -0400

    imported the save attributes action and dispatch to AdminEditUserPage and passed as a prop to the component

--- frontend/src/pages/admin/AdminEditProductPage.js
+++ frontend/src/pages/admin/AdminEditProductPage.js
@@ -4,6 +4,10 @@ import { useSelector } from "react-redux";
 
 import axios from "axios";
 
+// Redux for Saving Attribute to Category:
+import { useDispatch } from "react-redux";
+import { saveAttributeToCatDoc } from "../../redux/actions/categoriesAction";
+
 // Get Method: Fetch Products according to id:
 const fetchProduct = async (productId) => {
     const { data } = await axios.get(`/api/products/get-one/${productId}`);
@@ -23,7 +27,15 @@ const AdminEditProductPage = () => {
     //Fetch Categories from Redux state for categories dropdown:
     const { categories } = useSelector((state) => state.getCategories);
 
-    return <EditProductPageComponent categories={categories} fetchProduct={fetchProduct} updateProductApiRequest={updateProductApiRequest} />
+    const reduxDispatch = useDispatch();
+
+    return <EditProductPageComponent
+        categories={categories}
+        fetchProduct={fetchProduct}
+        updateProductApiRequest={updateProductApiRequest}
+        reduxDispatch={reduxDispatch}
+        saveAttributeToCatDoc={saveAttributeToCatDoc}
+    />
 };
 
 export default AdminEditProductPage;
