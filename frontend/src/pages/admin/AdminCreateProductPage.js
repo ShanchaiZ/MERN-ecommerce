4f31a836fa16484fed027d225deb26515e5b6919
ShanchaiZ <shanchai.zahid@gmail.com>
2023-06-24 16:55:00 -0400

    Created functions that serve newly created product data. explanation of code: We create a new create product api request that takes form Inputs as arguments which are targeted form fields. We make a post type request to this api endpoint and return the data. We make a similar function regarding the images as well. We create another function that uploadImages api request that takes in images and productId as arguments. We target the FormData () which is a built in js class used for handling forms and array from images. Then we create an array of images and for each image we append to the images key. Then do a post type request to the api endpoint and the formdata is the payload. Then pass the 2 functions as props to the component.

--- frontend/src/pages/admin/AdminCreateProductPage.js
+++ frontend/src/pages/admin/AdminCreateProductPage.js
@@ -1,8 +1,28 @@
 import CreateProductPageComponent from "./components/CreateProductPageComponent";
 
+import axios from "axios";
+
+// Create a new Product using Form Fields:
+const createProductApiRequest = async (formInputs) => {
+    const { data } = await axios.post(`/api/products/admin`, { ...formInputs });
+    return data;
+}
+
+// Create a new Product that appends images to the newly created product:
+const uploadImagesApiRequest = async (images, productId) => {
+    const formData = new FormData();
+    Array.form(images).forEach(image => {
+        formData.append("images", image);
+    })
+    await axios.post("/api/products/admin/upload?productId=" + productId, formData);
+}
+
+
 const AdminCreateProductPage = () => {
 
-    return <CreateProductPageComponent />
+    return <CreateProductPageComponent
+        createProductApiRequest={createProductApiRequest}
+        uploadImagesApiRequest={uploadImagesApiRequest} />
 };
 
 export default AdminCreateProductPage;
