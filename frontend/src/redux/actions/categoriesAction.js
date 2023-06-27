4bae7ff694dd26b0f62616e8a7d64233c63985ab
ShanchaiZ <shanchai.zahid@gmail.com>
2023-06-26 23:51:53 -0400

    Created a new categoryConstant, action and reducer that Deletes Category in the Category dropdown. Explanation of code: We create a new constant called delete category. In the action Categories: - We get all the available list of category in the state. - Then we filter for 1 item and we return the name of the category that is not the same name as the item argument. Then the filtered result is passed to the redux state.
    -Then we do a delete type request to the api endpoint and we encode the name in case the category name have slashes.
    -If there is a data DeletecCategory then we dispatch the action of deleting the category.
    In the category reducer:
    we add another case of delete category which returns the old state and overridden category with the action payload (which is the filtered categories)

--- frontend/src/redux/actions/categoriesAction.js
+++ frontend/src/redux/actions/categoriesAction.js
@@ -34,3 +34,16 @@ export const newCategory = (category) => async (dispatch, getState) => {
         })
     }
 }
+
+// Function: Deleting Category in the Category dropdown X:
+export const deleteCategory = (category) => async (dispatch, getState) => {
+    const cat = getState().getCategories.categories;
+    const categories = cat.filter((item) => item.name !== category);
+    const { data } = await axios.delete("/api/categories/" + encodeURIComponent(category));
+    if (data.categoryDeleted) {
+        dispatch({
+            type: actionTypes.DELETE_CATEGORY,
+            payload:[...categories],
+        })
+    }
+}
