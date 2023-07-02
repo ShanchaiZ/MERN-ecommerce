9922a88fa104d465827f75fb92c8c16dc4f736d2
ShanchaiZ <shanchai.zahid@gmail.com>
2023-07-02 14:06:52 -0400

    Dynamically display the image zoom and configuration. Explanation of code: Placed the image display zoom configuration in an if block that returns dynamic zoom effect. It takes product.images and maps the array of images using image and id as arguments. This results in zoom on the image that is targeted by getElementById and uses a string template literal to locate the dynamic id. After it finds the image to zoom in it applies the options which are defined above.

--- frontend/src/pages/components/ProductDetailsPageComponent.js
+++ frontend/src/pages/components/ProductDetailsPageComponent.js
@@ -25,22 +25,21 @@ const ProductDetailsPageComponent = ({ addToCartReduxAction, reduxDispatch, getP
         setShowCartmessage(true);
     }
 
-
+    // Product image display and configuration:
     useEffect(() => {
-        //Zoomed effects configuration:
-        var options = {
-            width: 400, // width of the source image (optional)
-            zoomWidth: 500, //width of the zoomed image. Zoomed image height equals source image height (optional)
-            // fillContainer: true,
-            // zoomPosition: "bottom", //the position of the zoomed image
-            scale: 1.5, //Zoom scale. if not provided, scale is calculated as natural image size / image size, provided in params (optional if zoomWidth param is provided)
-            offset: { vertical: 0, horizontal: 10 } //Zoomed image offset displayed offset the original hover
-        };
-        // The Zoomed Effects on Images:
-        new ImageZoom(document.getElementById("first"), options);
-        new ImageZoom(document.getElementById("second"), options);
-        new ImageZoom(document.getElementById("third"), options);
-        new ImageZoom(document.getElementById("fourth"), options);
+        if (product.images) {
+            //Zoomed effects configuration:
+            var options = {
+                width: 400, // width of the source image (optional)
+                zoomWidth: 500, //width of the zoomed image. Zoomed image height equals source image height (optional)
+                // fillContainer: true,
+                // zoomPosition: "bottom", //the position of the zoomed image
+                scale: 1.5, //Zoom scale. if not provided, scale is calculated as natural image size / image size, provided in params (optional if zoomWidth param is provided)
+                offset: { vertical: 0, horizontal: 10 } //Zoomed image offset displayed offset the original hover
+            };
+            // The Zoomed Effects on Dynamic Images:
+            product.images.map((image, id) => new ImageZoom(document.getElementById(`imageId${id + 1}`), options));
+        }
     })
 
     // Display Product Details:
