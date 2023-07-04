c15718ff023f8818d1ac77f4c3bee0f66f27fb92
ShanchaiZ <shanchai.zahid@gmail.com>
2023-07-03 22:49:24 -0400

    Seperated Home page into logic and component page for easier testing of components

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,32 +1,9 @@
-import ProductCarouselComponent from "../components/ProductCarouselComponent";
-import CategoryCardComponent from "../components/CategoryCardComponent";
-import { Row, Container } from "react-bootstrap";
+import HomePageComponent from "./components/HomePageComponent";
+
 
 const HomePage = () => {
-    // Simulating a database once we have a db connected in the future below:
-    const categories = [
-        "Tablets",
-        "Monitors",
-        "Games",
-        "Software",
-        "Electronics",
-        "Books",
-        "Videos",
-        "Computers"
-    ];
 
-    return (
-        <>
-            <ProductCarouselComponent />
-            <Container>
-                <Row xs={1} md={2} className="g-4 mt-3">
-                    {categories.map((category, idx) => (
-                        <CategoryCardComponent key={idx} category={category} idx={idx} />
-                    ))}
-                </Row>
-            </Container>
-        </>
-    );
+    return <HomePageComponent />
 };
 
 export default HomePage;
---
+++ frontend/src/pages/components/HomePageComponent.js
@@ -0,0 +1,32 @@
+import ProductCarouselComponent from "../../components/ProductCarouselComponent";
+import CategoryCardComponent from "../../components/CategoryCardComponent";
+import { Row, Container } from "react-bootstrap";
+
+const HomePageComponent = () => {
+    // Simulating a database once we have a db connected in the future below:
+    const categories = [
+        "Tablets",
+        "Monitors",
+        "Games",
+        "Software",
+        "Electronics",
+        "Books",
+        "Videos",
+        "Computers"
+    ];
+
+    return (
+        <>
+            <ProductCarouselComponent />
+            <Container>
+                <Row xs={1} md={2} className="g-4 mt-3">
+                    {categories.map((category, idx) => (
+                        <CategoryCardComponent key={idx} category={category} idx={idx} />
+                    ))}
+                </Row>
+            </Container>
+        </>
+    );
+};
+
+export default HomePageComponent;
