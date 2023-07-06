ef908cf6f7bac9effe1954eba2e2338bec7c07e8
ShanchaiZ <shanchai.zahid@gmail.com>
2023-07-05 22:36:23 -0400

    completed rating from attribute filter by returning the star items when clicked on state change

--- frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
+++ frontend/src/components/filterQueryResultOptions/RatingFilterComponent.js
@@ -9,7 +9,13 @@ const RatingFilterComponent = ({ setRatingsFromFilter }) => {
             {Array.from({ length: 5 }).map((_, idx) => (
                 <Fragment key={idx}>
                     <Form.Check type="checkbox" id={`check-api-${idx}`}>
-                        <Form.Check.Input type="checkbox" isValid />
+                        <Form.Check.Input
+                            type="checkbox"
+                            isValid
+                            onChange={(e) =>
+                                setRatingsFromFilter((items) => {
+                                    return { ...items, [5 - idx]: e.target.checked }
+                                })} />
                         <Form.Check.Label style={{ cursor: "pointer" }}>
                             {/* Star Rating Filter: read only */}
                             {<Rating readonly size={20} initialValue={5 - idx} />}
