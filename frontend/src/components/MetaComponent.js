707373ca9b1da2a5a9c3c955ea50a37547fb19a1
ShanchaiZ <shanchai.zahid@gmail.com>
2023-07-22 16:54:07 -0400

    Created a dynamic Meta Component in the Frontend. Explanation of code: To further improve SEO on this app, create a separate meta component that has title (which is the name of the tab of the app) and description (is the content property of the meta description found in the index.html) Import Helmet and Helmet Provider. Inside the Helmet tag, add the dynamic title and meta description and value for the content property. Then export default meta component to be implemented in app.

---
+++ frontend/src/components/MetaComponent.js
@@ -0,0 +1,15 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+const MetaComponent = ({ title, description }) => {
+    return (
+        <HelmetProvider>
+            <Helmet>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+            </Helmet>
+        </HelmetProvider>
+    )
+}
+
+
+export default MetaComponent;
