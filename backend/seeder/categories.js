9305ecdd9cbca9af5c9c0a988a1f7a3195555757
ShanchaiZ <shanchai.zahid@gmail.com>
2023-08-09 18:23:34 -0400

    Updated the categories seeder

--- backend/seeder/categories.js
+++ backend/seeder/categories.js
@@ -2,7 +2,7 @@ const categories = [
     {
         name: "Computers",
         description:
-            "Computers - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "The ByteBliss - Whether you're a tech enthusiast, a professional, or a student embarking on a digital journey, explore our range of devices designed to elevate your digital experience.",
         image: "/images/category/computers-category.jpg",
         attrs: [
             { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
@@ -10,9 +10,9 @@ const categories = [
         ],
     },
     {
-        name: "Computers/Laptops/Dell",
+        name: "Laptops",
         description:
-            "Computers - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "Laptop Oasis - Unleash your potential with our curated lineup of laptops. From compact companions for on-the-go productivity to high-performance beasts for intensive tasks, find the perfect blend of power and portability that fits your digital lifestyle.",
         image: "/images/category/computers-category.jpg",
         attrs: [
             { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
@@ -20,19 +20,19 @@ const categories = [
         ],
     },
     {
-        name: "Computers/Laptops/Lenovo",
+        name: "Sports and Outdoors",
         description:
-            "Computers - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "Atheletics Collection - Embrace an active lifestyle with our curated selection of sports equipment and outdoor essentials.",
         image: "/images/category/computers-category.jpg",
         attrs: [
-            { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
+            { key: "Gear", value: ["indoors", "camping", "recreational"] },
             { key: "color", value: ["blue", "red", "green", "black"] },
         ],
     },
     {
         name: "Tablets",
         description:
-            "Tablets - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "Tablet Haven - Immerse yourself in the perfect blend of portability and functionality with our range of tablets. Experience the freedom to work, play, and connect on a sleek and versatile device that's your gateway to endless digital exploration.",
         image: "/images/category/tablets-category.jpg",
         attrs: [
             { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
@@ -40,51 +40,95 @@ const categories = [
         ],
     },
     {
-        name: "Videos",
+        name: "Appliances",
         description:
-            "Videos - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "Appliance Emporium - From kitchen marvels that transform cooking to smart home solutions that simplify everyday tasks, discover a collection that enhances comfort and convenience in every corner of your home.",
         image: "/images/category/videos-category.jpg",
         attrs: [
-            { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
+            { key: "Warranty", value: ["1 year", "2 year", "3 year"] },
             { key: "color", value: ["blue", "red", "green", "black"] },
         ],
     },
     {
         name: "Books",
         description:
-            "Books - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "The Literary Escapes - Dive into a world of words and imagination with our vast collection of books spanning every genre and topic.",
         image: "/images/category/books-category.jpg",
         attrs: [
-            { key: "genre", value: ["movie", "comedy", "thriller"] }
+            { key: "Literary Genre", value: ["Fiction", "Non-Fiction", "Mystery", "Fantasy", "Science", "Humor", "Romance"] }
         ],
     },
     {
         name: "Software",
         description:
-            "Softwares - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "Software Hub - Explore a realm of digital possibilities with our curated software collection. From productivity tools that streamline your tasks to creative applications that amplify your artistic flair, discover software solutions that empower your digital endeavors.",
         image: "/images/category/software-category.jpg",
+        attrs: [
+            { key: "Compatibility", value: ["With Windows Products", "With Apple Products", "All of the above"] },
+            { key: "RAM", value: ["1 GB", "2 GB", "4 GB"] },
+        ],
     },
     {
         name: "Electronics",
         description:
-            "Electronics - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "ElectroVerse - From smart gadgets that simplify your daily routine to cutting-edge tech that enhances your connectivity, explore a universe of electronic marvels designed to elevate your lifestyle.",
         image: "/images/category/electronics-category.jpg",
+        attrs: [
+            { key: "Ownership", value: ["Brand New", "Previously Owned", "Refurbished"] },
+            { key: "Warranty", value: ["1 year", "2 year", "3 year"] },
+        ],
     },
     {
         name: "Games",
         description:
-            "Games - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "Game Galaxy - Step into a realm of endless entertainment with our captivating collection of games. Choose from a diverse array of genres and platforms, and embark on unforgettable gaming journeys that ignite your imagination.",
         image: "/images/category/games-category.jpg",
         attrs: [
-            { key: "Rating", value: ["E", "T", "M", "R"] }
+            { key: "Rating", value: ["E", "T", "M", "R"] },
+            { key: "Platform", value: ["PC", "Playstation", "Xbox"] },
         ],
     },
     {
         name: "Monitors",
         description:
-            "Monitors - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+            "Monitor Haven - Elevate your visual experience with our curated selection of monitors. Explore a range of sizes, resolutions, and features, and find the perfect monitor to immerse yourself in stunning visuals.",
         image: "/images/category/monitors-category.jpg",
+        attrs: [
+            { key: "Screen Size", value: ["24 inch", "27 inch", "30 inch"] },
+            { key: "Resolution", value: ["Full HD (1080p)", "Quad HD (1440p)"] }
+        ],
     },
+    {
+        name: "Health & Personal Care",
+        description:
+            "Wellness Corner: Nurture your wellbeing and Explore a curated selection of items designed to support your self-care journey, from skincare essentials that radiate confidence to wellness products that prioritize your vitality.",
+        image: "/images/category/computers-category.jpg",
+        attrs: [
+            { key: "Relaxation", value: ["Aromatherapy", "Message Tools"] },
+            { key: "color", value: ["blue", "red", "green", "black"] },
+        ],
+    },
+    {
+        name: "Office Products",
+        description:
+            "Office Oasis - From our collection of ergonomic furniture and essential stationery to high-tech gadgets, discover everything you need to optimize efficiency and inspire creativity in your office environment.",
+        image: "/images/category/computers-category.jpg",
+        attrs: [
+            { key: "Eco-friendly", value: ["Made with recycled products", "Energy Efficient", "Fair Trade"] },
+            { key: "color", value: ["blue", "red", "green", "black"] },
+        ],
+    },
+    // Extra Categories template:
+    // {
+    //     name: "Computers/Laptops/Lenovo",
+    //     description:
+    //         "Computers - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, magnam qui. Accusamus similique placeat at nisi autem eaque molestiae velit.",
+    //     image: "/images/category/computers-category.jpg",
+    //     attrs: [
+    //         { key: "RAM", value: ["1 TB", "2 TB", "4 TB"] },
+    //         { key: "color", value: ["blue", "red", "green", "black"] },
+    //     ],
+    // },
 ]
 
 
