54c5cd305086db54b415ae794066c16bc1d6bcc7
ShanchaiZ <shanchai.zahid@gmail.com>
2023-09-19 18:42:38 -0400

    created a basic suit of a counter increment/decrement click fire event. Test passed as expected

--- frontend/src/utils/exampleComponent.test.js
+++ frontend/src/utils/exampleComponent.test.js
@@ -5,5 +5,14 @@ import Counter from "./exampleComponent";
 test("counter test, not part of the e-commerce app", () => {
     render(<Counter />)
     expect(screen.getByText("Decrement")).toBeInTheDocument();
+    const increment = screen.getByText("Increment");
+    const decrement = screen.getByText("Decrement");
+    const message = screen.getByText(/Current count/);
+    fireEvent.click(increment);
+    expect(message.textContent).toBe("Current count: 1");
+    fireEvent.click(decrement); // result = "0"
+    fireEvent.click(decrement); // result = "-1"
+    fireEvent.click(decrement); // result = "-2"
+    expect(message.textContent).toBe("Current count: -2");
 })
 
